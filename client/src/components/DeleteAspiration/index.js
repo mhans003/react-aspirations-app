a4e6799ed482bd7a9665f586c39efb8a13338f7a
mhans003 <michaeledwardhanson@gmail.com>
2021-01-08 10:30:56 -0800

    Confirm delete modal has correct styling for heading as well as correct color and icon for delete button.

--- client/src/components/DeleteAspiration/index.js
+++ client/src/components/DeleteAspiration/index.js
@@ -18,20 +18,20 @@ function DeleteAspiration(props) {
             <Modal show={props.deleteAspirationShow} onHide={props.handleDeleteAspirationClose}>
                 <Modal.Header closeButton className="dim-background">
                     <Modal.Title>
-                        <h2>Confirm Delete</h2>
+                        <h2 className="font-light squeezed">Confirm Delete</h2>
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body className="show-grid">
                     <div>
                         <form className="form-group my-3" onSubmit={handleSubmit}>
-                            <h3>Are you sure you want to delete this aspiration and all its milestones?</h3>
+                            <h3 className="font-light squeezed">Are you sure you want to delete this aspiration and all its milestones?</h3>
                             <hr/>
                             <p><strong>{props.title}</strong></p>
-                            <p>{props.description}</p>
-                            <button className="btn btn-primary btn-lg btn-block mt-3 mb-3" type="submit" onClick={() => {
+                            <p><i>{props.description}</i></p>
+                            <button className="btn btn-danger btn-lg btn-block mt-3 mb-3" type="submit" onClick={() => {
                                 props.handleDeleteAspirationClose();
                             }}>
-                                Delete <i className="fal fa-save"></i>
+                                Delete <i className="fas fa-trash-alt"></i>
                             </button>
                         </form>
                         <hr className="mb-4"/>
