16cb2bd987fb53fd1235a59cb58ff9b444e8dd82
mhans003 <michaeledwardhanson@gmail.com>
2021-01-08 11:59:24 -0800

    Testing Heroku Deploy: BroswerRouter declared before components

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,7 +7,6 @@ import Aspirations from "./pages/Aspirations";
 import Admin from "./pages/Admin";
 import PrivateRoute from "./hocs/PrivateRoute";
 import PublicRoute from "./hocs/PublicRoute";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./pages/style.css"
 
 function App() {
